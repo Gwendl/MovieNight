c2f85a50a85f895dd3c95817198c3745b8f2c52b
Adrien morel <adrien.morel@me.com>
2016-04-26 18:09:43 -0700

    basic movie list gui

---
+++ MovieNight/MovieListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MovieListViewController.swift
+//  MovieNight
+//
+//  Created by Adrien morel on 4/26/16.
+//  Copyright © 2016 Gwendal Lasson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieListTableViewController : UITableViewController {
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("movieCell", forIndexPath: indexPath) 
+        
+        return cell
+    }
+}
