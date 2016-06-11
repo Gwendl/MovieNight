a45adb546844b3b1cbfe0a46d35ae52edddb123a
Adrien morel <adrien.morel@me.com>
2016-06-11 12:24:14 +0200

    load movie thumbails in list

--- MovieNight/MovieListViewController.swift
+++ MovieNight/MovieListViewController.swift
@@ -12,6 +12,7 @@ import UIKit
 class MovieListTableViewController : UITableViewController {
     
     var movies: NSArray = []
+    var posters: [UIImage?]?
     
     override func viewDidLoad() {
         let api = MovieNightAPI(partnerKey: "100043982026", secretKey: "29d185d98c984a359e6e6f26a0474269")
@@ -21,6 +22,8 @@ class MovieListTableViewController : UITableViewController {
     func populateTableView(data: NSArray) {
         
         movies = data
+        print(movies)
+        posters = [UIImage?](count: movies.count, repeatedValue: nil)
         tableView.reloadData()
     }
     
@@ -35,8 +38,58 @@ class MovieListTableViewController : UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let name = "movieCell"
-        let cell = tableView.dequeueReusableCellWithIdentifier(name, forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier(name, forIndexPath: indexPath) as! MovieTableViewCell
+        
+        let movieData = movies[indexPath.row]
+        
+        let title = movieData["title"] as! String
+        cell.movieName.text = title
+        
+        let poster = movieData["poster"] as! NSDictionary
+        let posterLink = poster["href"] as! String
+        
+        cell.moviePoster.image = nil
+        
+        let imageView = cell.moviePoster
+        
+        func setImage() {
+            
+            UIView.transitionWithView(imageView,
+                                      duration:1,
+                                      options: UIViewAnimationOptions.TransitionCrossDissolve,
+                                      animations: { imageView.image = self.posters![indexPath.row] },
+                                      completion: nil)
+        }
+        
+        if let poster = posters![indexPath.row] {
+            
+            cell.moviePoster.image = poster
+            
+        } else {
+            
+            if let url = NSURL(string: posterLink) {
+                let request = NSURLRequest(URL: url)
+                let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+                let session = NSURLSession(configuration: config)
+                
+                let task = session.dataTaskWithRequest(request, completionHandler: {(data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+                    
+                    if let imageData = data as NSData? {
+                        dispatch_async(dispatch_get_main_queue(), {() -> Void in
+                            self.posters![indexPath.row] = UIImage(data: imageData)
+                            setImage()
+                        })
+                    }
+                })
+                
+                task.resume()
+            }
+            
+        }
         
         return cell
     }
+    
+    
+    
 }
---
+++ MovieNight/MovieTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieTableViewCell.swift
+//  MovieNight
+//
+//  Created by Adrien Morel on 11/06/16.
+//  Copyright © 2016 Gwendal Lasson. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieName: UILabel!
+    @IBOutlet weak var movieDuration: UILabel!
+    @IBOutlet weak var moviePoster: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
