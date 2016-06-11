be6fdea4c508f05071b3985e8bbb0ff6abc22a47
Adrien morel <adrien.morel@me.com>
2016-06-11 10:07:18 +0200

    show master viewcontroller first in SplitView

--- MovieNight/ViewController.swift
+++ MovieNight/ViewController.swift
@@ -9,34 +9,22 @@
 import UIKit
 
 
-class MainSplitView: UISplitViewController {
+class MainSplitView: UISplitViewController, UISplitViewControllerDelegate {
 
     static let appColor = UIColor(red: 0xFF / 255, green: 0x2D / 255, blue: 0x55 / 255, alpha: 0x00 / 255)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        let url = NSURL(string: "http://www.stackoverflow.com")
-        
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
-            print(NSString(data: data!, encoding: NSUTF8StringEncoding))
-        }
-        
-        task.resume()
-
-        
-        
-        // Do any additional setup after loading the view, typically from a nib.
-
-        
+        self.delegate = self
     }
-
+    
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController, ontoPrimaryViewController primaryViewController: UIViewController) -> Bool {
+        return true
+    }
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
